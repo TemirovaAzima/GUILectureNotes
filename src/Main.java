509a59108679c6081d8a60d7288f4148e0bf3ff7
TemirovaAzima <temirovaazima9@gmail.com>
2026-03-24 23:59:32 +0100

    Animal class added and overwritten with anonymous class

--- src/Main.java
+++ src/Main.java
@@ -115,7 +115,50 @@ public class Main {
         while (!stInt.empty())
             System.out.println(stInt.pop() + " ");
         System.out.println();
+        BiIntOperator[] opers = {
+                // objects of concrete classes
+                // implementing an interface
+                new AddAndMult(2),
+               new MultAndAdd(5),
+                // object of anonymous class
+                // implementing an interface
+                new BiIntOperator(){
+                    @Override
+                    public int apply(int i, int j){
+                        return  i*i + j*j;
+                    }
+                },
+                // object of anonymous class
+                // extending a regular class
+                new MultAndAdd(3){
+                    @Override
+                    public int apply(int i , int j){
+                        return seed*(i*i + j*j);
+                    }
+                }
+        };
+        int a = 1, b =2;
+        for (BiIntOperator op : opers)
+            System.out.print(op.apply(a,b) + " ");
+        System.out.println();
+        Animal max = new Animal("Max",15) {
+            @Override
+            public String speak() {
+                return "bow-wow";
+            }
+        };
 
+        Animal[] animals = {
+                max,
+                new Animal("Batty",3.5) {
+                    @Override
+                    public String speak() {
+                        return "miaou-miaou";
+                    }
+                }
+        };
+        for(Animal t: animals)
+            System.out.println(t);
     }
 
     static void writeL(Person[] list, String header) {
